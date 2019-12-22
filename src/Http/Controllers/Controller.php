0f77875f2daa40daf93aeee8ef1c0fead191552d
Softpay <lrxp2008@hotmail.com>
2019-12-22 14:31:02 -0300

    1.10.2
    Atualização do Controller.

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -5,11 +5,23 @@ namespace Softpay\ Parcelamento\ Http\ Controllers;
 use App\ Http\ Controllers\ Controller;
 use Illuminate\ Http\ Request;
 
+use Softpay\Parcelamento\Models\Parcelamento;
+
 class Controller extends Controller {
 	
-	public function index() {
+	# Função para gerar os recebíveis de uma transação
+	public function gerarRecebiveis() {
+		
+		return Parcelamento::gerarRecebiveis();
 		
 	}	
+	
+	# Função para simular o parcelamento de uma venda
+	public function simularParcelamento(){
+		
+		return Parcelamento::simularParcelamento();
+		
+	}
 	
 	
 }
--- src/ParcelamentoServiceProvider.php
+++ src/ParcelamentoServiceProvider.php
@@ -4,9 +4,16 @@ namespace Softpay\Parcelamento;
 
 use Illuminate\Support\ServiceProvider;
 
+
+/*********************************************************************************************************
+ * Os provedores de serviços são o local central de toda a inicialização do aplicativo Laravel.          *
+ * Seu próprio aplicativo, bem como todos os serviços principais do Laravel, são inicializados por       *
+ * meio de provedores de serviços.                                                                       *
+ ********************************************************************************************************/
+
 class ParcelamentoServiceProvider extends ServiceProvider
 {
-    
+    # Comandos que serão utilizados
     protected $commands = [
         'Softpay\Parcelamento\Commands\NewPackage',
         'Softpay\Parcelamento\Commands\RemovePackage',
@@ -22,15 +29,22 @@ class ParcelamentoServiceProvider extends ServiceProvider
     
     public function boot()
     {
+		# Inicializando o arquivo de parcelamento junto com o pacote
         $this->publishes([
             __DIR__.'/../config/Parcelamento.php' => config_path('Parcelamento.php'),
         ]);
+		
     }
     
     public function register()
     {
+		# Registrando o arquivo de configuração do nosso pacote
         $this->mergeConfigFrom(__DIR__.'/../config/Parcelamento.php', 'Parcelamento');
         $this->commands($this->commands);
+		
+		# Registrando nosso controller
+		$this->app->make('Softpay\Parcelamento\Http\Controllers\CalculatorController');
+		
     }
 	
 }
