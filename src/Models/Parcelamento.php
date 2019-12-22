8657324b4fbbf5c188f96bcd0162d55e54d9f8f9
Softpay <lrxp2008@hotmail.com>
2019-12-22 10:38:12 -0300

    1.0.7
    Atualização da classe Parcelamento, sendo incluso as funções.

--- src/Models/Parcelamento.php
+++ src/Models/Parcelamento.php
@@ -6,4 +6,39 @@ use Illuminate\ Database\ Eloquent\ Model;
 
 class Parcelamento extends Model {
 	
+	# Declaração das variáveis
+	private $parcelasSemJuros;
+    private $maxParcelas;
+    private $valorMinimoParcelar;
+    private $porcentagemJuros = 0;
+	
+	# Método construtor
+	public function __construct($parcelas = 0, $parcelasSemJuros = 0, $parcelaMinima = 0, $porcentagemMDR = 0){
+		
+        $this->parcelas          = $parcelas;
+        $this->parcelasSemJuros  = $parcelasSemJuros;
+        $this->parcelaMinima     = $parcelaMinima;
+        $this->porcentagemMDR    = $porcentagemMDR;
+    }
+	
+	# Função para gerar as parcelas
+	public function gerarParcelas(){
+		
+	}
+	
+	# Cálculo de parcelas sem juros
+	private function parcelasSemJuros(){
+		
+	}
+	
+	# Cálculo de parcelas com juros
+	private function parcelasComJuros(){
+		
+	}
+	
+	# Cálculo do máximo de parcelas, dependendo do valor
+	private function maximoParcelas(){
+		
+	}
+	
 }
