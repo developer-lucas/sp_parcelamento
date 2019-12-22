874833a7e08861be2aeed0885a572c244ed25b9b
Softpay <lrxp2008@hotmail.com>
2019-12-22 10:11:17 -0300

    1.0.6
    Adiciona o restante da estrutura.

---
+++ src/Http/Controllers/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Softpay\ Parcelamento\ Http\ Controllers;
+
+use App\ Http\ Controllers\ Controller;
+use Illuminate\ Http\ Request;
+
+class Controller extends Controller {
+	
+	public function index() {
+		
+	}	
+	
+	
+}
---
+++ src/Models/Parcelamento.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Softpay\ Parcelamento\ Models;
+
+use Illuminate\ Database\ Eloquent\ Model;
+
+class Parcelamento extends Model {
+	
+}
