79a24e57e38d786ec26b22c747b821de3ff4c66e
Softpay <lrxp2008@hotmail.com>
2020-01-07 13:35:40 -0300

    Update ParcelamentoServiceProvider.php

--- src/ParcelamentoServiceProvider.php
+++ src/ParcelamentoServiceProvider.php
@@ -16,38 +16,12 @@ use Softpay\Parcelamento\Validation;
 
 class ParcelamentoServiceProvider extends ServiceProvider
 {
-    # Comandos que serão utilizados
-    protected $commands = [
-        'Softpay\Parcelamento\Commands\NewPackage',
-        'Softpay\Parcelamento\Commands\RemovePackage',
-        'Softpay\Parcelamento\Commands\GetPackage',
-        'Softpay\Parcelamento\Commands\GitPackage',
-        'Softpay\Parcelamento\Commands\ListPackages',
-        'Softpay\Parcelamento\Commands\MoveTests',
-        'Softpay\Parcelamento\Commands\CheckPackage',
-        'Softpay\Parcelamento\Commands\PublishPackage',
-        'Softpay\Parcelamento\Commands\EnablePackage',
-        'Softpay\Parcelamento\Commands\DisablePackage',
-    ];
     
-    public function boot()
-    {
-		if ($this->app->runningInConsole()) {
-			
-			$this->publishes([
-            	__DIR__.'/../config/Parcelamento.php' => config_path('Parcelamento.php'),
-			]);
-			
-		};
+    public function boot() {
 		
     }
     
-    public function register()
-    {
-				
-		# Registrando o arquivo de configuração do nosso pacote
-        $this->mergeConfigFrom(__DIR__.'/../config/Parcelamento.php', 'Config');
-        $this->commands($this->commands);
+    public function register() {
 		
 		$this->app->bind(TemporaryFileFactory::class, function () {
             return new TemporaryFileFactory(
